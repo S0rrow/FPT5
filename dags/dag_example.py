ec1c73fa1e93e87e95b4833972535dddbc722716
S0rrow <jae9hell@gmail.com>
2024-08-09 14:53:56 +0900

    seperated volume and volume mount variables

--- dags/dag_example.py
+++ dags/dag_example.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
 from datetime import timedelta
 
 default_args = {
@@ -10,6 +11,17 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+volume_mount = k8s.V1VolumeMount(
+    name="airflow-worker-pvc-mount",
+    mount_path="/mnt/data/airflow",
+    sub_path=None,
+    read_only=True
+)
+
+volume = k8s.V1Volume(
+    name="airflow-worker-pvc",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="airflow-worker-pvc"),
+)
 
 with DAG(
     dag_id='run_script_from_pvc_dag',
@@ -24,18 +36,6 @@ with DAG(
         cmds=["python", "/mnt/data/airflow/test.py"],
         name='run-script',
         task_id='run_script_from_pvc',
-        volume_mounts=[
-            {
-                'name': 'airflow-worker-pvc',
-                'mountPath': '/mnt/data/airflow'
-            }
-        ],
-        volumes=[
-            {
-                'name': 'airflow-worker-pvc',
-                'persistentVolumeClaim': {
-                    'claimName': 'airflow-worker-pvc'
-                }
-            }
-        ]
+        volume_mounts=volume_mount,
+        volumes=volume
     )
