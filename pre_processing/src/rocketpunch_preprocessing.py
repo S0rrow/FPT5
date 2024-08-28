26434432c7836816f164fdb74ffefbe78e0046d7
bonabanq <seoyeon.lee.da@gmail.com>
2024-08-28 10:11:39 +0900

    [UPDATE] #54 modify code

--- pre_processing/src/rocketpunch_preprocessing.py
+++ pre_processing/src/rocketpunch_preprocessing.py
@@ -9,6 +9,7 @@ from json.decoder import JSONDecodeError
 from botocore.exceptions import ClientError
 import re
 from decimal import Decimal
+import logging
 
 from src import utils
 '''
@@ -41,53 +42,51 @@ def import_bucket(key, bucket_info):
     s3 = session.client('s3')
     
     # 샘플데이터만 처리
-    #response = s3.list_objects_v2(Bucket=pull_bucket_name, Prefix=target_folder_prefix, Delimiter='/')
-    response = s3.get_object(Bucket=pull_bucket_name, Key='rocketpunch/data/2024-08-21_1702.json')
-    data = response['Body'].read()
+    #response = s3.get_object(Bucket=pull_bucket_name, Key='rocketpunch/data/2024-08-21_1702.json')
+    #data = response['Body'].read()
 
     # JSON으로 변환
-    json_objects = []
-    for line in data.splitlines():
-        try:
-            json_objects.append(json.loads(line))
-        except json.JSONDecodeError as e:
-            print(f"JSONDecodeError: {e} - in line: {line}")
-
-    #print('load JSON from S3')
-    return json_objects
-     
-    # # 특정 폴더 내 파일 목록 가져오기
-    # # TODO: 
-    # # - 마지막 실행일(년,월,일)을 datetime으로 저장한 파일을 읽어들여 curr_date에 적용하기; 당담: 유정연
-    # response = s3.list_objects_v2(Bucket=pull_bucket_name, Prefix=target_folder_prefix, Delimiter='/')
-    # curr_date = datetime.datetime.now(pytz.timezone('Asia/Seoul')).date()  # 로컬 시간대(UTC+9)로 현재 날짜 설정
-    # kst_tz = pytz.timezone('Asia/Seoul') # kst timezone 설정
-    # #curr_date = datetime.date(2024, 8, 21)
-
-    # # curr_date 보다 날짜가 늦은 data josn 파일 metadata 객체 분류
-    # if 'Contents' in response:
-    #     target_file_list = [obj for obj in response['Contents'] if curr_date <= obj['LastModified'].astimezone(kst_tz).date()]
-    # else:
-    #     print("No objects found in the folder.")
-        
-    # for obj in target_file_list:
+    # json_objects = []
+    # for line in data.splitlines():
     #     try:
-    #         response = s3.get_object(Bucket=pull_bucket_name, Key=obj['Key'])
-    #         json_context = response['Body'].read().decode('utf-8')
-    #         cleaned_text = re.sub(r'[\r\u2028\u2029]+', ' ', json_context) # 파싱을 위해 unuseal line terminators 제거
-    #         json_list = [json.loads(line) for line in cleaned_text.strip().splitlines()] # pandas format으로 맞추기
-    #         data = pd.DataFrame(json_list)
-    #     except JSONDecodeError as e:
-    #         logging.error(f"JSONDecodeError encountered: {e}")
-    #         continue
-    #     except ClientError as e:
-    #         logging.error(f"ClientError encountered: {e}")
-    #         continue
-    #     except Exception as e:
-    #         logging.error(f"An unexpected error occurred: {e}")
-    #         continue
-    #     break
-    #    return data
+    #         json_objects.append(json.loads(line))
+    #     except json.JSONDecodeError as e:
+    #         print(f"JSONDecodeError: {e} - in line: {line}")
+
+    # return json_objects
+     
+    # 특정 폴더 내 파일 목록 가져오기
+    # TODO: 
+    # - 마지막 실행일(년,월,일)을 datetime으로 저장한 파일을 읽어들여 curr_date에 적용하기; 당담: 유정연
+    response = s3.list_objects_v2(Bucket=pull_bucket_name, Prefix=target_folder_prefix, Delimiter='/')
+    curr_date = datetime.datetime.now(pytz.timezone('Asia/Seoul')).date()  # 로컬 시간대(UTC+9)로 현재 날짜 설정
+    kst_tz = pytz.timezone('Asia/Seoul') # kst timezone 설정
+    #curr_date = datetime.date(2024, 8, 21)
+
+    # curr_date 보다 날짜가 늦은 data josn 파일 metadata 객체 분류
+    if 'Contents' in response:
+        target_file_list = [obj for obj in response['Contents'] if curr_date <= obj['LastModified'].astimezone(kst_tz).date()]
+    else:
+        print("No objects found in the folder.")
+        
+    for obj in target_file_list:
+        try:
+            response = s3.get_object(Bucket=pull_bucket_name, Key=obj['Key'])
+            json_context = response['Body'].read().decode('utf-8')
+            cleaned_text = re.sub(r'[\r\u2028\u2029]+', ' ', json_context) # 파싱을 위해 unuseal line terminators 제거
+            json_list = [json.loads(line) for line in cleaned_text.strip().splitlines()] # pandas format으로 맞추기
+            data = pd.DataFrame(json_list)
+        except JSONDecodeError as e:
+            logging.error(f"JSONDecodeError encountered: {e}")
+            continue
+        except ClientError as e:
+            logging.error(f"ClientError encountered: {e}")
+            continue
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+            continue
+        break
+        return data
 
 '''
   1차 전처리를 진행합니다.
@@ -181,7 +180,6 @@ def main():
     # df에 값이 있으면 파싱 처리
     if df: 
         preprocessing(df, key)
-        #export_dynamo(processing_dict, key)
     else:
         print('No task for preprocessing.')
     
