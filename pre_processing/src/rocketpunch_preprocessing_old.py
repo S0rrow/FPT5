e1d52fae0dd9ed50d0ef9c46288e2090034df335
bonabanq <seoyeon.lee.da@gmail.com>
2024-08-29 15:05:03 +0900

    [UPDATE] #54 modify logics to import from s3 bucket

--- pre_processing/src/rocketpunch_preprocessing_old.py
+++ pre_processing/src/rocketpunch_preprocessing_old.py
@@ -17,13 +17,13 @@ import utils
  local에서 엑세스 키를 가져옵니다.  
 '''
 def access_keys(path):
+    os.listdir(path)
     with open(f'{path}/API_KEYS.json', 'r') as f:
         key = json.load(f)
     with open(f'{path}/DATA_SRC_INFO.json', 'r') as f:
         bucket_info = json.load(f)
         
     return key, bucket_info
-
 '''
   S3 버킷에서 데이터를 가져옵니다.
   버킷의 모든 데이터를 가져오고, archive 쪽에 적재합니다.
@@ -31,7 +31,9 @@ def access_keys(path):
 '''
 def import_bucket(key, bucket_info):
     pull_bucket_name = bucket_info['pull_bucket_name']
+    target_folder_prefix = bucket_info['target_folder_prefix']['rocketpunch_path']
     dump_bucket_name = bucket_info['dump_bucket_name']
+    
     # S3 섹션 및 client 생성
     session = boto3.Session(
         aws_access_key_id=key['aws_access_key_id'],
@@ -41,28 +43,41 @@ def import_bucket(key, bucket_info):
 
     s3 = session.client('s3')
     
-    response = s3.list_objects_v2(Bucket=pull_bucket_name, Prefix='rocketpunch')
-    print(f'response: {response}')
-    if 'Contents' not in response:
+    response = s3.list_objects_v2(Bucket=pull_bucket_name, Prefix=target_folder_prefix, Delimiter='/')
+    #response = s3.get_object(Bucket=pull_bucket_name, Key='rocketpunch/data/2024-08-21_1702.json')
+    #data = response['Body'].read()
+    
+    # json_objects = []
+    # for line in data.splitlines():
+    #     try:
+    #         json_objects.append(json.loads(line))
+    #     except json.JSONDecodeError as e:
+    #         print(f"JSONDecodeError: {e} - in line: {line}")
+    
+    #print('load JSON from S3')
+    #return json_object
+    
+    # 특정 폴더 내 파일 목록 가져오기
+    # TODO: 
+    # - 마지막 실행일(년,월,일)을 datetime으로 저장한 파일을 읽어들여 curr_date에 적용하기; 당담: 유정연
+    response = s3.list_objects_v2(Bucket=pull_bucket_name, Prefix=target_folder_prefix, Delimiter='/')
+    curr_date = datetime.datetime.now(pytz.timezone('Asia/Seoul')).date()  # 로컬 시간대(UTC+9)로 현재 날짜 설정
+    kst_tz = pytz.timezone('Asia/Seoul') # kst timezone 설정
+    #curr_date = datetime.date(2024, 8, 21)
+    
+    # curr_date 보다 날짜가 늦은 data josn 파일 metadata 객체 분류
+    if 'Contents' in response:
+        target_file_list = [obj for obj in response['Contents'] if curr_date <= obj['LastModified'].astimezone(kst_tz).date()]
+    else:
         print("No objects found in the folder.")
-        return None
-
-    all_data = []
-
-    for obj in response['Contents']:
+        
+    for obj in target_file_list:
         try:
-            s3_response = s3.get_object(Bucket=pull_bucket_name, Key=obj['Key'])
-            json_context = s3_response['Body'].read().decode('utf-8')
+            response = s3.get_object(Bucket=pull_bucket_name, Key=obj['Key'])
+            json_context = response['Body'].read().decode('utf-8')
             cleaned_text = re.sub(r'[\r\u2028\u2029]+', ' ', json_context) # 파싱을 위해 unuseal line terminators 제거
             json_list = [json.loads(line) for line in cleaned_text.strip().splitlines()] # pandas format으로 맞추기
             data = pd.DataFrame(json_list)
-            all_data.append(data)
-
-            # 데이터 처리 후, 파일을 다른 버킷으로 복사 후 삭제
-            copy_source = {"Bucket": pull_bucket_name, "Key": obj['Key']}
-            s3.copy(copy_source, dump_bucket_name, obj['Key'])
-            s3.delete_object(Bucket=pull_bucket_name, Key=obj['Key'])
-
         except JSONDecodeError as e:
             logging.error(f"JSONDecodeError encountered: {e}")
             continue
@@ -72,20 +87,17 @@ def import_bucket(key, bucket_info):
         except Exception as e:
             logging.error(f"An unexpected error occurred: {e}")
             continue
+        break
+        return data
     
-    if all_data:
-        return pd.concat(all_data, ignore_index=True)
-    else:
-        return None
-
 '''
   1차 전처리를 진행합니다.
   제이슨을 한 줄씩 읽고 한 줄씩 DynamoDB로 전송해 데이터를 넣습니다.
 '''
 def preprocessing(df, key):
-    print("preprocessing start")
-    for i, data in df.iterrows() :
-        print(data)
+    #data_df = pd.DataFrame(df)
+    
+    for i, data in data_df.iterrows() :
         processing_dict = {}
         # rocketpunch key > merged key
         # job_task > job_tasks
@@ -113,7 +125,7 @@ def preprocessing(df, key):
             date_end_stp = int(date_end.timestamp())
             processing_dict['end_date'] = date_end_stp
         
-        # job_career > required_career
+        #job_career > required_career
         processing_dict['required_career'] = any(career in "신입" for career in data['job_career'])
         
         # symbol 추가
@@ -135,7 +147,6 @@ def preprocessing(df, key):
     
         #### dynamoDB에 바로 저장
         export_dynamo(processing_dict, key)
-
 '''
   DynamoDB로 전처리한 데이터를 적재합니다.
 '''
@@ -164,7 +175,7 @@ def export_dynamo(processing_dict, key):
     table.put_item(Item=processing_dict) 
     
 def main():
-    file_path = '/home/team3/repository/keys/'  # local file path
+    file_path = '/home/team3/repository/keys/' #local file path
     key, bucket_info = access_keys(file_path)
     df = import_bucket(key, bucket_info)
  
@@ -174,5 +185,5 @@ def main():
     else:
         print('No task for preprocessing.')
     
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
