9ebc15ec364ff6b2b864aa1f664f2e098828a5a1
JeungYeun Yoo <yjyj989812@naver.com>
2024-09-05 17:51:23 +0900

    [INITIAL] #107 create new dag: 2nd-preprocessing and update codes to connect first preprocessing dags and second preprocessing dag

--- dags/dag_programmers.py
+++ dags/dag_programmers.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.sensors.sqs import SqsSensor
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
 from airflow.utils.dates import days_ago
@@ -70,7 +71,7 @@ with DAG(
         region_name='ap-northeast-2',
     )
     
-    anolize_message = PythonOperator(
+    start_analyze_message = PythonOperator(
         task_id='analyze_message',
         python_callable=analyze_message,
         provide_context=True,
@@ -86,6 +87,7 @@ with DAG(
         volume_mounts=[volume_mount],
         volumes=[volume],
         dag=dag,
+        xcom_push=True,
         trigger_rule='all_success',  # 이전 작업이 성공하면 실행
     )
     
@@ -96,4 +98,12 @@ with DAG(
         trigger_rule='all_success',  # first_preprocessing이 성공했을 때만 실행
     )
     
-    wait_for_message >> anolize_message >> first_preprocessing >> delete_message
+    trigger_2nd_preprocessing = TriggerDagRunOperator(
+        task_id='trigger_second_preprocessing',
+        trigger_dag_id='second_preprocessing',   # 실행할 second_preprocessing DAG의 DAG ID
+        conf={"records": "{{ task_instance.xcom_pull(task_ids='first_preprocessing_programmers') }}"},  # XCom 출력값 전달
+        wait_for_completion=False,  # True로 설정하면 second_preprocessing DAG가 완료될 때까지 현재 DAG 대기
+        trigger_rule='all_success',
+    )
+    
+    wait_for_message >> start_analyze_message >> first_preprocessing >> delete_message >> trigger_2nd_preprocessing
