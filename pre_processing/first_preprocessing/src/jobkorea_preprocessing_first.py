441092fb4d431c2e4a1886569daffa74a125603b
JeungYeun Yoo <yjyj989812@naver.com>
2024-09-05 12:46:27 +0900

    [UPDATE][FIX] #113 Add new feature on jobkorea 1st pre-processing code: id duplications drop features using redis instead s3

--- pre_processing/first_preprocessing/src/jobkorea_preprocessing_first.py
+++ pre_processing/first_preprocessing/src/jobkorea_preprocessing_first.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import sys, re, pytz, json, boto3
+import sys, re, pytz, json, boto3, redis
 
 from jobkorea import jobkorea
 from logging_to_cloudwatch import log
@@ -56,8 +56,12 @@ def main():
     pull_bucket_name = storage_info['pull_bucket_name']
     data_archive_bucket_name = storage_info['crawl_data_bucket_name']
     push_table_name = storage_info['restore_table_name']
-    id_list_bucket_name = storage_info['id_storage_bucket_name']
+    #id_list_bucket_name = storage_info['id_storage_bucket_name']
     target_folder_prefix = storage_info['target_folder_prefix']['jobkorea_path']
+    redis_id = storage_info['redis_conn_info']['id']
+    redis_port = storage_info['redis_conn_info']['port']
+    redis_sassion = redis.StrictRedis(host=redis_id, port=redis_port, db=0)
+    
     # 특정 폴더 내 파일 목록 가져오기
     # TODO: 
     # - 마지막 실행일(년,월,일)을 datetime으로 저장한 파일을 읽어들여 curr_date에 적용하기; 당담: 유정연
@@ -79,11 +83,14 @@ def main():
             instance = jobkorea(logger)
             result_df = instance.pre_processing_first(json_list)
             unique_df = result_df.drop_duplicates(subset='id', keep='first')
-            upload_record_ids = utils.remove_duplicate_id(s3, id_list_bucket_name, unique_df)
-            filtered_df = unique_df[unique_df['id'].isin(upload_record_ids)]
+            #upload_record_ids = utils.remove_duplicate_id(s3, id_list_bucket_name, unique_df)
+            upload_ids_records = utils.check_id_in_redis(logger, redis_sassion, unique_df.to_dict(orient='records'))
+            filtered_df = unique_df[unique_df['id'].isin([record['id'] for record in upload_ids_records])]
             if len(filtered_df):
                 upload_data(logger,filtered_df,aws_key,push_table_name)
-                update_respone = utils.update_ids_to_s3(s3, id_list_bucket_name, "obj_ids.json", upload_record_ids)
+                utils.upload_id_into_redis(logger, redis_sassion, upload_ids_records)
+                print(json.dumps(upload_ids_records)) # Airflow DAG Xcom으로 값 전달하기 위해 stdout 출력 
+                #update_respone = utils.update_ids_to_s3(s3, id_list_bucket_name, "obj_ids.json", upload_record_ids)
         except Exception as e:
             s3.copy({"Bucket":data_archive_bucket_name,"Key":obj["Key"]},pull_bucket_name,obj["Key"])
             logger.error(f"{obj['Key']} upload error")
